f84d0fa95da91aa0ffdd2de0af46becff33f98b3
913165 <tinumistry@gmail.com>
2022-11-14 02:07:23 +0530

    Update ScheduleService.java

--- ignitedropcreatepopulate/src/main/java/com/example/ignitedropcreatepopulate/schedule/ScheduleService.java
+++ ignitedropcreatepopulate/src/main/java/com/example/ignitedropcreatepopulate/schedule/ScheduleService.java
@@ -14,11 +14,19 @@ public class ScheduleService {
     @Autowired
     IgniteService igniteService;
 
-    @Scheduled(fixedDelay = 1000*60*60, initialDelay = 5000)
+    @Scheduled(fixedDelay = 1000*60*10, initialDelay = 5000)
     public void ddlService() {
         log.info("ddlService started");
         igniteService.dropAndCreateTable();
         log.info("ddlService ended");
+    }
+
+    @Scheduled(fixedDelay = 1000*60*25, initialDelay = 1000*60*20)
+    public void deleteandinsert() {
+        log.info("ddlService started");
+        igniteService.dropAndCreateTable();
+        log.info("ddlService ended");
+        log.info("-------------------------------------------------------------------");
     }
 }
 
